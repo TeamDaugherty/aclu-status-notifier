6af1e39d73a7da64019f98bc45f1b043a3a8c4b6
cboath <cteater@gmail.com>
2021-09-08 22:29:17 -0500

    Breaking things out

--- client/src/components/rightPane.js
+++ client/src/components/rightPane.js
@@ -1,48 +1,19 @@
-import '../App.js';
-import React from 'react';
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import '../App.js'
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Login from './Login.js'
 
-function handleSubmit() {
-      
-}
+// function handleSubmit() {}
 
 class RightPane extends React.Component {
-
-    render() {
-      return (
-        <div className="mainRight">
-          <div className="loginFrame">
-            <h3>Sign In</h3>
-            <p>Sign in to eStat by entering your email address and password below. 
-              If this is your first time logging into eStat after verifying your 
-              email address, use the Forgot Password process to update your password.</p>
-          </div>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group size="lg" controlId="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                autoFocus
-                type="email"
-                value="myemail"
-                //onChange={}
-              />
-            </Form.Group>
-            <Form.Group size="lg" controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                value="notthis"
-                //onChange={}
-              />
-            </Form.Group>
-        <Button block size="lg" type="submit" >
-          Login
-        </Button>
-      </Form>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className='mainRight'>
+        <Login />
+      </div>
+    )
   }
+}
 
-  export default RightPane
+export default RightPane
