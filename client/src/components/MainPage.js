95594378c73c1770d99504d1a82c6ca7efe1a76a
justinbergkamp <justin.bergkamp@gmail.com>
2021-10-28 14:00:41 -0500

    Fixing step displa

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -12,7 +12,7 @@ class MainPage extends React.Component {
   render() {
     return (
       <div >
-        <Login />
+        <ComplaintFlow />
       </div>
     )
   }
--- client/src/components/StepDisplay.js
+++ client/src/components/StepDisplay.js
@@ -3,18 +3,27 @@ import React from 'react';
 class StepDisplay extends React.Component {
 
   renderSwitch(param) {
-  switch(param) {
-    case 1:
-      return <div className="stepText">Enter</div>
-    default:
-      return <div className="stepText">Add</div>
-    }
+    switch (param.stepNumber) {
+      case 1:
+        return <div className="stepText">Enter</div>
+      case 2:
+        return <div className="stepText">Add</div>
+      case 3:
+        return <div className="stepText">Send Update</div>
+      case 4:
+        return <div className="stepText">Success</div>
+      default:
+        return <div className="stepText">Add</div>
+      }
   }
 
   render() {
+    const { stepNumber } = this.props;
+
     return(
       <div className="stepDisplay">
-        <div className="dot">{this.props.stepNumber}</div>
+        <div className="dot">{stepNumber}</div>
+        <div> {this.renderSwitch({stepNumber})} </div>
       </div>
     );
   }
