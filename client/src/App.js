2ee5ed483023f2c8304bb60620266e58e7eb2914
Andy Mulchek <a.mulchek@gmail.com>
2021-12-05 17:18:22 -0600

    add login and sign out capabilities

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import './App.css'
 import MainPage from './components/MainPage'
-import Login from './components/Login/Login'
+
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>ACLU</header>
-        <div >
-          <Login />
-        </div>
+      <header className='App-header'>
+          <div className='header-title'>
+            ACLU
+          </div>
+          <div className='header-action'>
+            <AmplifySignOut />
+          </div>
+
+      </header>
+      <div >
+        <MainPage />
+      </div>
     </div>
   )
 }
 
-export default App
+export default withAuthenticator(App);
