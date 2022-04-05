9b9b35487ed3e892e675d2c80d9bdb964a719076
Andy Mulchek <a.mulchek@gmail.com>
2022-04-05 16:25:57 -0500

    Update styles and flow

---
+++ client/src/components/EnterComplaint/Statuses.js
@@ -0,0 +1,8 @@
+const statuses = [
+    "Complaint Received/Assigned to Complaint Counselor",
+    "Complaint Escalated to Legal For Secondary Review",
+    "Complaint Converted to Case",
+    "Complaint Closed"
+]
+
+export default statuses;
--- client/src/components/EnterComplaint/Success.js
+++ client/src/components/EnterComplaint/Success.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react'
 import Stepper from '../Stepper'
-import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import steps from './steps'
 
 export class AddComplaint extends Component {
-  continue = e => {
-    e.preventDefault();
-    this.props.nextStep();
-  };
-
-  back = e => {
-  e.preventDefault();
-  this.props.prevStep();
-};
-
-
   render() {
-    const { values, handleChange } = this.props;
+    const { values, nextStep, clearComplaintId } = this.props;
+
+    const restart = async (e) => {
+        e.preventDefault();
+        clearComplaintId();
+        nextStep(steps.enterComplaintId);           
+    };
 
     return (
       <div className="container">
         <div className="complaintFrame column">
           <Stepper stepNumber={values.step}/>
           <h1>Success</h1>
+          <p>
+              The status of <span style={{fontWeight: "bold"}}>{values.complaintID}</span> has been updated.
+          </p>
+          <Button variant="primary" onClick={restart} className="submit-btn">
+            Add/Update Another Complaint
+          </Button>
       </div>
       </div>
     );
